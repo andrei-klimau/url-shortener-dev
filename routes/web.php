f8b6f88b8f42cacf43251d5cddbbc414fbf9c4d6
Andrei Klimau <andrew.iv79@gmail.com>
2024-04-15 14:03:09 +0300

    Improving web route code

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,16 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RedirectController;
 use App\Http\Controllers\ShortUrlController;
 use App\Http\Controllers\TokenController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-/*
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-*/
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home');
 
-Route::resource('urls', ShortUrlController::class)->middleware('auth');
+Route::resource('/urls', ShortUrlController::class)->middleware('auth');
 
 Route::controller(TokenController::class)->middleware('auth')->group(function () {
     Route::get('/tokens', 'index')->name('tokens.index');
